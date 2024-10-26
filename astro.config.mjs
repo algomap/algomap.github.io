c50b19fd4e8c39a20df29de16262fc952e989cc5
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2024-10-26 14:49:50 +0200

    adding i18n support for the future + static arrays page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeMathJax from 'rehype-mathjax';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://algomap.github.io',
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeMathJax],
+	},
 	integrations: [
 		starlight({
 			title: 'AlgoMap',
-
+			customCss: ['./src/mathjax.css'],
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
@@ -21,10 +27,18 @@ export default defineConfig({
 					],
 				},
 				{
+					label: 'Strutture Dati',
+					autogenerate: { directory: 'ds' },
+				},
+				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
 	],
+	i18n: {
+		defaultLocale: "it",
+		locales: ["it", "en"]
+	}
 });
--- src/content/docs/reference/example.md
+++ src/content/docs/reference/example.md
@@ -1,11 +1,10 @@
 ---
-title: Example Reference
+title: Riferimento d'esempio
 description: A reference page in my new Starlight docs site.
 ---
 
-Reference pages are ideal for outlining how things work in terse and clear terms.
-Less concerned with telling a story or addressing a specific use case, they should give a comprehensive outline of what you're documenting.
+Questo è il documento in italiano
 
-## Further reading
+## Further readingbro
 
 - Read [about reference](https://diataxis.fr/reference/) in the Diátaxis framework
